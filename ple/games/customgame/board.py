659ca17fb66bf55493b7bd2d4e79912d2b9506ed
Vashisht Madhavan <vashisht@uber.com>
2018-10-29 09:59:56 -0700

    fixing number of fires + holes for easy

--- ple/games/customgame/board.py
+++ ple/games/customgame/board.py
@@ -205,7 +205,7 @@ class Board(object):
             self.map[self.map == 20] = 0 # removing init princess position
             self.map[self.map == 11] = 0 # removing init enemy position
 
-            numFires = int(np.abs(gauss(self.vec[0] + 1, 1.0)))
+            numFires = int(np.abs(gauss(self.vec[0], 1.0)))
             numEnemies = int(np.abs(gauss(self.vec[1], 1.0)))
             positions = [tuple(y) for y in np.argwhere(self.map == 1)]
             positions = self.removeInvalidPositions(positions)
